bfaf7d52a5facee02cdac8dcb8e8750054956ff1
lkvr44 <lukavarava@yandex.ru>
2024-12-13 22:37:30 +0300

    Update ping-pong_game.py added while (not working yet)

--- ping-pong_game.py
+++ ping-pong_game.py
@@ -8,6 +8,8 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = pl_x
         self.rect.y = pl_y
+        self.side = 'right'
+        self.sidey = 'up'
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
@@ -25,11 +27,37 @@ class Player(GameSprite):
         if keys_pressed[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
 
+class Ball(GameSprite):
+    def update(self):
+        if self.side == 'right':
+            if self.rect.x < 510:
+                self.rect.x += self.speed
+            else:
+                self.rect.x -= self.speed
+                self.side = 'left'
+        elif self.side == 'left':
+            if self.rect.x > 5:
+                self.rect.x -= self.speed
+            else:
+                self.rect.x += self.speed
+                self.side = 'right'
+        if self.sidey == 'up':
+            if self.rect.y < 510:
+                self.rect.y += self.speed
+            else:
+                self.rect.y -= self.speed
+                self.sidey = 'down'
+        elif self.sidey == 'down':
+            if self.rect.y > 5:
+                self.rect.y -= self.speed
+            else:
+                self.rect.y += self.speed
+                self.sidey = 'up'
 background = (200, 255, 255)
 wind_width = 600
 wind_height = 500
-widow = display.set_mode((wind_width, wind_height))
-widow.fill(background)
+window = display.set_mode((wind_width, wind_height))
+window.fill(background)
 
 game = True
 finish = False
@@ -38,7 +66,7 @@ FPS = 60
 
 player_l = Player('racket.png' ,30, 200, 5, 50, 150)
 player_r = Player('racket.png' ,520, 300, 5, 50, 150)
-ball = GameSprite('tenis_ball.png' ,200, 200, 4, 50, 50)
+ball = Ball('tenis_ball.png' ,200, 200, 4, 50, 50)
 
 speed_x = 3
 speed_y = 3
@@ -48,11 +76,46 @@ font= font.SysFont('Arial', 70)
 win_l = font.render('Left WIN!', True, (255, 200, 0))
 win_r = font.render('Right LOSE!', True, (255, 200, 0))
 
+left_score=0
+right_score=0
+
+while game:
+    for e in event.get():
+            if e.type == QUIT:
+                game= False
 
 
+    if not finish:
 
+        window.blit(background,(0,0))
 
+        player_l.update_l()
+        player_r.update_r()
+        ball.update()
 
+        ball.reset()
+
+        text = font.render('Счет: ' + str(left_score) + '/3', 1, (255,255,255))
+        window.blit(text,(10,20))
+
+        text_lose = font.render('Пропущено: ' + str(right_score) + '/3', 1, (255,255,255))
+        window.blit(text_lose,(10,50))
+
+        clock.tick(FPS)
+
+        if left_score >= 3:
+            text = font.render('Счет: ' + str(left_score) + '/3', 1, (255,255,255))
+            window.blit(text,(10,20))
+            finish = True
+            window.blit(win_l, (200,200))
+
+        if right_score >= 3:
+            text_lose = font.render('Пропущено: ' + str(right_score) + '/3', 1, (255,255,255))
+            window.blit(text_lose,(10,50))
+            finish = True
+            window.blit(win_r, (200,200))
+
+    display.update()
 
 
 
